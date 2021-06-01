059e9dcd68c2798a45177ea96584eaed56c37a99
Wim Berchmans <wim.berchmans@porphyrio.com>
2021-06-01 17:04:44 +0200

    Just flatten all leaves

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::env::args;
 use std::fs::rename;
 use std::error::Error;
 use walkdir::WalkDir;
-use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let first_argument: String = args().nth(1).unwrap();
+
     for entry in WalkDir::new(&first_argument)
         .follow_links(true)
         .into_iter()
@@ -13,13 +13,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     {
         let f_name = entry.file_name().to_string_lossy();
         let p_name = String::from(entry.path().to_string_lossy());
-        let sec = entry.metadata()?.modified()?;
 
-        if f_name.ends_with(".pdf") && sec.elapsed()?.as_secs() < 86400 {
-            let new = format!("{}/{}", &first_argument, &f_name);
-            println!("Renaming from {} to {}", &p_name, &new);
-            rename(&p_name, &new).expect("Unable to rename");
-        }
+        let new = format!("{}/{}", &first_argument, &f_name);
+        println!("Renaming from {} to {}", &p_name, &new);
+        rename(&p_name, &new).expect("Unable to rename");
     }
 
     Ok(())
