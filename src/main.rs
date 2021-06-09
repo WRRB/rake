dc5852266439f413e1a7f77858ce527a71a1900e
Wim Berchmans <wim.berchmans@porphyrio.com>
2021-06-09 22:35:24 +0200

    Change name, add cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use std::env::args;
 use std::fs::rename;
 use std::error::Error;
 use walkdir::WalkDir;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rake", about= "Gather leaves")]
+struct Cli {
+    directory: String
+}
+
+// TODO: prompt user if number of files is high
+// TODO: prompt user if dir is home dir
+// TODO: prompt user by default, use flag to disable prompt
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let first_argument: String = args().nth(1).unwrap();
+    let args: Cli = Cli::from_args();
+    
 
-    for entry in WalkDir::new(&first_argument)
+    for entry in WalkDir::new(&args.directory)
         .follow_links(false)
         .into_iter()
         .filter_map(|e| e.ok())
@@ -14,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let f_name = entry.file_name().to_string_lossy();
         let p_name = String::from(entry.path().to_string_lossy());
 
-        let new = format!("{}/{}", &first_argument, &f_name);
+        let new = format!("{}/{}", &args.directory, &f_name);
         println!("Renaming from {} to {}", &p_name, &new);
         rename(&p_name, &new).expect("Unable to rename");
     }
